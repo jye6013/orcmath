2fef8cd1bc2d6cc7893aeabfea01cf9fefe633cb
yejaz <yejaz@192.168.0.14>
2018-01-09 17:55:55 -0500

    a

--- OrcMathGUI/src/simon/SimonGameJi.java
+++ OrcMathGUI/src/simon/SimonGameJi.java
@@ -1,23 +1,28 @@
 package simon;
 
 import guiTeacher.GUIApplication;
+import holiday.Front;
+import holiday.HolidayCard;
 
 public class SimonGameJi extends GUIApplication{
 	//
-	public static SimonScreenJi simon;
-
+	public static SimonGameJi simon;
+	public static SimonScreenJi simonScreen;
+	
 	public SimonGameJi(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		setVisible(true);
 	}
 
 	@Override
 	public void initScreen() {
-		// TODO Auto-generated method stub
+		simonScreen = new SimonScreenJi(getWidth(), getHeight());
+		setScreen(simonScreen);
 		
 	}
 	
 	public static void main(String[] args) {
+		simon = new SimonGameJi(800, 500);
 		Thread runner = new Thread(simon);
 		runner.start();
 	}
